232b8536cc051dc0ecf9907b6ce45039f104c66d
Gheorghe Bulicanu <gicu@live.com>
2024-08-10 01:38:25 +0300

    Use flask api from react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <button className='btn btn-primary'>
+        <button type='button' className='btn btn-primary'>
           Start Personality Test
         </button>
+        <hr />
+        <p className='h6'>The current time is {new Date(currentTime*1000).toTimeString()}.</p>
       </header>
     </div>
   );
