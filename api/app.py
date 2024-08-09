3bd79d673989471dbea0500c44602e6c5f3c4e92
Gheorghe Bulicanu <gicu@live.com>
2024-08-10 01:11:21 +0300

    Add flask api backend

---
+++ api/app.py
@@ -0,0 +1,12 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
