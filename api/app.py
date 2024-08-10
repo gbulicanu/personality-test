ebaff5f8d9f82ee735651c45e396bc8bb33a8979
Gheorghe Bulicanu <gicu@live.com>
2024-08-10 04:08:36 +0300

    Deploy frontend from python flask api

--- api/app.py
+++ api/app.py
@@ -3,7 +3,12 @@ from flask import Flask
 
 from questions import QUESTIONS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route('/api/time')
