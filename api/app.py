52bad080d88d7ace8bdb46abd232fa0ca4664168
Gheorghe Bulicanu <gicu@live.com>
2024-08-10 03:28:13 +0300

    Add questions crud

--- api/app.py
+++ api/app.py
@@ -1,12 +1,28 @@
 import time
 from flask import Flask
 
+from questions import QUESTIONS
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/questions')
+def get_all_questions():
+    # get all questions
+    questions = []
+    for question in QUESTIONS:
+        question_view = question.copy()
+        question_view.pop('answers')
+        questions.append(question_view)
+    return questions
+
+
+@app.route('/questions/<int:question_id>')
+def get_question(question_id):
+    # get question with the given id
+    return next((q for q in QUESTIONS if q['id'] == question_id), None) or {}, 404
---
+++ api/questions.py
@@ -0,0 +1,52 @@
+QUESTIONS = [
+    {
+        'id': 1,
+        'title': 'You''re really busy at work and a colleague is telling you their life story and personal woes. You:',
+        'answers': [
+            {'option': 'A', 'title': 'Don''t dare to interrupt them'},
+            {'option': 'B', 'title': 'Think it''s more important to give them some of your time; work can wait'},
+            {'option': 'C', 'title': 'Listen, but with only with half an ear'},
+            {'option': 'D', 'title': 'Interrupt and explain that you are really busy at the moment'},
+        ]
+    },
+    {
+        'id': 2,
+        'title': 'You''ve been sitting in the doctor''s waiting room for more than 25 minutes. You:',
+        'answers': [
+            {'option': 'A', 'title': 'Look at your watch every two minutes'},
+            {'option': 'B', 'title': 'Bubble with inner anger, but keep quiet'},
+            {'option': 'C', 'title': 'Explain to other equally impatient people in the room that the doctor is always running late'},
+            {'option': 'D', 'title': 'Complain in a loud voice, while tapping your foot impatiently'},
+        ]
+    },
+    {
+        'id': 3,
+        'title': 'You''re having an animated discussion with a colleague regarding a project that you''re in charge of. You:',
+        'answers': [
+            {'option': 'A', 'title': 'Don''t dare contradict them'},
+            {'option': 'B', 'title': 'Think that they are obviously right'},
+            {'option': 'C', 'title': 'Defend your own point of view, tooth and nail'},
+            {'option': 'D', 'title': 'Continuously interrupt your colleague'},
+        ]
+    },
+    {
+        'id': 4,
+        'title': 'You are taking part in a guided tour of a museum. You:',
+        'answers': [
+            {'option': 'A', 'title': 'Are a bit too far towards the back so don’t really hear what the guide is saying'},
+            {'option': 'B', 'title': 'Follow the group without question'},
+            {'option': 'C', 'title': 'Make sure that everyone is able to hear properly'},
+            {'option': 'D', 'title': 'Are right up the front, adding your own comments in a loud voice'},
+        ]
+    },
+    {
+        'id': 5,
+        'title': 'During dinner parties at your home, you have a hard time with people who:',
+        'answers': [
+            {'option': 'A', 'title': 'Ask you to tell a story in front of everyone else'},
+            {'option': 'B', 'title': 'Talk privately between themselves'},
+            {'option': 'C', 'title': 'Hang around you all evening'},
+            {'option': 'D', 'title': 'Always drag the conversation back to themselves'},
+        ]
+    },
+]
